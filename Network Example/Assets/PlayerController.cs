b4e35a6d8ccd618cae2d043060ad094739b29311
William Hoskins <hoskins.w.h@gmail.com>
2017-11-03 08:13:08 -0400

    Got Network Interpolation working

--- Network Example/Assets/PlayerController.cs
+++ Network Example/Assets/PlayerController.cs
@@ -3,17 +3,38 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    [SyncVar(hook = "SyncRot")]
+    private Vector3 rot;
+
     void Update()
     {
         if(!isLocalPlayer)
-        {
             return;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 100f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+
+        GetComponent<Rigidbody>().velocity = z * transform.forward;
+
+        Cmd_Rot(x);
+        //SyncRot();
+            //transform.Rotate(0, x, 0);
+            //transform.Translate(0, 0, z);
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+    [Command]
+    void Cmd_Rot(float x)
+    {
+        if(x != 0f)
+        {
+            rot = new Vector3(0, x, 0);
+        }
+    }
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+    void SyncRot(Vector3 rot)
+    {
+        print("hit");
+        transform.Rotate(rot);
     }
 }
+
